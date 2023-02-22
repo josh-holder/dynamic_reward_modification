5cfd9acb0899f7dabd60065a2dc6925e7a286b0a
Josh Holder <josh.holder72@gmail.com>
2023-02-22 15:24:23 -0800

    avging plots

--- plot_train.py
+++ plot_train.py
@@ -24,6 +24,7 @@ def plot_train():
     parser.add_argument("-x", "--x-axis", help="X-axis", choices=["steps", "episodes", "time"], type=str, default="steps")
     parser.add_argument("-y", "--y-axis", help="Y-axis", choices=["success", "reward", "length"], type=str, default="reward")
     parser.add_argument("-w", "--episode-window", help="Rolling window size", type=int, default=100)
+    parser.add_argument("--avg", "--plot-avg", help="Plot the average of all runs in this folder (0 for false, 1 for true)", type=int, default=0)
 
     args = parser.parse_args()
 
@@ -84,10 +85,63 @@ def plot_train():
             x, y_mean = window_func(x, y, args.episode_window, np.mean)
             plt.plot(x, y_mean, linewidth=2, label=folder.split("/")[-1])
 
+    print(args.avg)
+    if args.avg: 
+        if args.x_axis != "steps": print("Not printing avg. because x axis is not steps.")
+        else: plotAvgLine(dirs,args,x_axis,y_axis)
+
     plt.legend()
     plt.tight_layout()
     plt.show()
 
+def plotAvgLine(dirs,args,x_axis,y_axis):
+    max_timesteps_of_any_run = 0
+
+    all_results = []
+
+    #grab all results
+    for folder in dirs:
+        try:
+            data_frame = load_results(folder)
+        except LoadMonitorResultsError:
+            continue
+        if args.max_timesteps is not None:
+            data_frame = data_frame[data_frame.l.cumsum() <= args.max_timesteps]
+
+        all_results.append(data_frame)
+
+        if max_timesteps_of_any_run < data_frame.l.cumsum().iloc[-1]:
+            max_timesteps_of_any_run = data_frame.l.cumsum().iloc[-1]
+        # try:
+        #     y = np.array(data_frame[y_axis])
+        # except KeyError:
+        #     print(f"No data available for {folder}")
+        #     continue
+        # x, _ = ts2xy(data_frame, x_axis)
+
+        # # Do not plot the smoothed curve at all if the timeseries is shorter than window size.
+        # if x.shape[0] >= args.episode_window:
+        #     # Compute and plot rolling mean with window of size args.episode_window
+        #     x, y_mean = window_func(x, y, args.episode_window, np.mean)
+        #     plt.plot(x, y_mean, linewidth=2, label=folder.split("/")[-1])
+    
+    mean_y_by_length = []
+    lengths = np.linspace(0,max_timesteps_of_any_run, 1000)
+    for length in lengths:
+        ys = []
+        for df in all_results:
+            relevant_df = df[df.l.cumsum() <= length]
+            if relevant_df.empty == False:
+                ys.append(relevant_df[y_axis].iloc[-1])
+        
+        if len(ys) > 0:
+            mean_y_by_length.append(sum(ys)/len(ys))
+        else: mean_y_by_length.append(0)
+
+    lengths, mean_y_rolling = window_func(np.array(lengths), np.array(mean_y_by_length), 5, np.mean)
+    avg_data = np.hstack((lengths.reshape(-1, 1), mean_y_rolling.reshape(-1,1)))
+    np.savetxt('avg_rollout.csv', avg_data, delimiter=',', header=f'{x_axis},{y_axis}')
+    plt.plot(lengths, mean_y_rolling, linewidth=2, label="Average rewards")
 
 if __name__ == "__main__":
     plot_train()
