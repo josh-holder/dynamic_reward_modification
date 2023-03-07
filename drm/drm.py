29da9bc41a1181718373c2eaaf3ba233f61f63f7
Michael W <mwen8@uw.edu>
2023-03-07 15:33:40 -0800

    changed drm.py

--- drm/drm.py
+++ drm/drm.py
@@ -188,13 +188,13 @@ class DRM(OffPolicyAlgorithm):
                 for critic_index in critic_indices_to_use:
                     features = self.critic_target.extract_features(replay_data.next_observations, self.critic_target.features_extractor)
                     next_q_values.append(self.critic_target.q_networks[critic_index](th.cat([features, next_actions], dim=1)))
-                    
+
                 next_q_values = th.cat(next_q_values, dim=1) #change tuple to single tensor
                 next_q_values, _ = th.min(next_q_values, dim=1, keepdim=True)
 
                 q_variance_scaling = self.get_q_variance_scaling(single_tensor_current_q_values)
-                shaped_rewards = replay_data.rewards + th.mul(q_variance_scaling,calc_shaping_rewards(replay_data.observations, next_actions))
-                shaped_rewards = replay_data.rewards
+                shaped_rewards = replay_data.rewards + th.mul(q_variance_scaling,calc_shaping_rewards(replay_data.observations, replay_data.actions))
+                # shaped_rewards = replay_data.rewards
 
                 target_q_values = shaped_rewards + (1 - replay_data.dones) * self.gamma * next_q_values
 
