65bad2a4a31839fea3d99f8d530a6ac7d2352684
Josh Holder <josh.holder72@gmail.com>
2023-03-09 16:58:38 -0800

    lower vel penalty

--- lunar_lander_reward_shaping.py
+++ lunar_lander_reward_shaping.py
@@ -46,7 +46,7 @@ def calc_shaping_rewards(state, action):
 	heuristic_action = th.clip(heuristic_action, -1, +1)
 
 	velocity = th.sqrt(th.square(state[:,2])+th.square(state[:,3]))
-	velocity_penalty = th.max(0.25*th.ones_like(velocity), velocity)
+	velocity_penalty = th.mul(th.max(0.25*th.ones_like(velocity), velocity), 0.5)
 
 	# calculate the difference between the algorithm action and the heuristic action
 	# currently this is just a naive method, where the reward is the total negative
